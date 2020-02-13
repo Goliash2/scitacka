92a2c34bd8f7a3ebe44302bbfbe924d4649433a7
Petr Hnyk <hnyk.petr@centrum.cz>
2020-02-13 16:11:11 +0100

    Added QR code scanner

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { ViewChild } from '@angular/core';
+import {ZXingScannerComponent} from '@zxing/ngx-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+
+  scannerActive = false;
+
+  @ViewChild('scanner', { static: false })
+  scanner: ZXingScannerComponent;
+  private theBackCamera: any;
+
+  /**
+   * Some method.
+   */
+  doSomething(): void {
+    this.scanner.device = this.getBackCamera();
+  }
+
+  /**
+   * Returns the back camera for ya.
+   */
+  getBackCamera() {
+    return this.theBackCamera;
+  }
+
+  startScanner() {
+    this.scannerActive = !this.scannerActive;
+  }
 
   constructor() {}
 
